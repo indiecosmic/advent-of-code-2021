643d88191a534fd62bab0552d3a1f5c08284cf53
Mattias Rudén <mattias@indiesoft.org>
2021-12-09 02:02:21 +0100

    Draft day 8

---
+++ AdventOfCode.Tests/TestBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Tests
+{
+    public abstract class TestBase<T, TU>
+    {
+        protected T Subject { get; }
+
+        public TestBase(TU input)
+        {
+            Subject = (T)Activator.CreateInstance(typeof(T), input);
+        }
+    }
+}
---
+++ AdventOfCode/Day08.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    public class Day08
+    {
+        private readonly string[] _input;
+
+        public Day08(string[] input = null)
+        {
+            _input = input ?? Input.ReadAllLines(GetType().Name);
+        }
+
+        public int Part1()
+        {
+            return 0;
+        }
+    }
+}
