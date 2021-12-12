5b8608b413a31d9dbc05e3037e27690ab45c4b35
Mattias Rudén <mattias@indiesoft.org>
2021-12-12 03:01:56 +0100

    Day 10

---
+++ AdventOfCode.Tests/Day10_Tests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AdventOfCode.Tests
+{
+    public class Day10_Tests : TestBase<Day10, string[]>
+    {
+        public Day10_Tests()
+        : base(new[] {
+            "[({(<(())[]>[[{[]{<()<>>",
+            "[(()[<>])]({[<{<<[]>>(",
+            "{([(<{}[<>[]}>{[]{[(<()>",
+            "(((({<>}<{<{<>}{[]{[]{}",
+            "[[<[([]))<([[{}[[()]]]",
+            "[{[{({}]{}}([{[{{{}}([]",
+            "{<[[]]>}<{[{[{[]{()[[[]",
+            "[<(<(<(<{}))><([]([]()",
+            "<{([([[(<>()){}]>(<<{{",
+            "<{([{{}}[<[[[<>{}]]]>[]]"})
+        {
+        }
+
+        [Fact]
+        public void Part1()
+        {
+            var result = Subject.Part1();
+            Assert.Equal(26397, result);
+        }
+
+        [Fact]
+        public void Part2()
+        {
+            var result = Subject.Part2();
+            Assert.Equal(288957, result);
+        }
+    }
+}
---
+++ AdventOfCode/Day10.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    public class Day10
+    {
+        private readonly string[] _input;
+        private const string Openers = "([{<";
+
+        private readonly string[] _pairs = new[]
+        {
+            "()",
+            "[]",
+            "{}",
+            "<>"
+        };
+
+        public Day10(string[] input = null)
+        {
+            _input = input ?? Input.ReadAllLines(GetType().Name);
+        }
+
+        public long Part1()
+        {
+            return _input.Sum(line => Score(line).score);
+        }
+
+        public long Part2()
+        {
+            var scores = new List<long>();
+            foreach (var line in _input)
+            {
+                var (score, remainder) = Score(line);
+                if (score == 0)
+                {
+                    while (remainder.TryPop(out var c))
+                    {
+                        var completionScore = CompletionScore(c);
+                        score *= 5;
+                        score += completionScore;
+                    }
+                    scores.Add(score);
+                }
+            }
+
+            scores.Sort();
+
+            return scores[scores.Count/2];
+        }
+
+        private (long score, Stack<char> remainder) Score(string line)
+        {
+            var chars = new Stack<char>();
+            foreach (var c in line)
+            {
+                if (Openers.Contains(c))
+                {
+                    chars.Push(c);
+                }
+                else
+                {
+                    if (Matches(chars.Peek(), c))
+                        chars.Pop();
+                    else
+                    {
+                        switch (c)
+                        {
+                            case ')':
+                                return (3, null);
+                            case ']':
+                                return (57, null);
+                            case '}':
+                                return (1197, null);
+                            case '>':
+                                return (25137, null);
+                        }
+                    }
+                }
+            }
+
+            return (0, chars);
+        }
+        
+        private bool Matches(char opener, char closer)
+        {
+            var str = new string(new[] { opener, closer });
+            return _pairs.Contains(str);
+        }
+
+        private int CompletionScore(char c)
+        {
+            return c switch
+            {
+                '(' => 1,
+                '[' => 2,
+                '{' => 3,
+                '<' => 4,
+                _ => throw new Exception()
+            };
+        }
+    }
+}
