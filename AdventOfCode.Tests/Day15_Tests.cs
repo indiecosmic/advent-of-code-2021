4af4fd5a0e28b46e599fb9206abf9cb905feb0ae
Mattias Rudén <mattias.ruden@sr.se>
2021-12-15 18:24:48 +0100

    Draft day 15

---
+++ AdventOfCode.Tests/Day15_Tests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AdventOfCode.Tests
+{
+    public class Day15_Tests : TestBase<Day15, string[]>
+    {
+        public Day15_Tests()
+            : base(new[]
+            {
+                "1163751742",
+                "1381373672",
+                "2136511328",
+                "3694931569",
+                "7463417111",
+                "1319128137",
+                "1359912421",
+                "3125421639",
+                "1293138521",
+                "2311944581"
+            })
+        {
+        }
+
+        [Fact]
+        public void Part1()
+        {
+            var result = Subject.Part1();
+            Assert.Equal(40, result);
+        }
+
+    }
+}
---
+++ AdventOfCode/Day15.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    public class Day15
+    {
+        private int[,] map;
+        private readonly (int x, int y) north = (x: 0, y: -1);
+        private readonly (int x, int y) south = (x: 0, y: 1);
+        private readonly (int x, int y) west = (x: -1, y: 0);
+        private readonly (int x, int y) east = (x: 1, y: 0);
+
+        public Day15(string[] input = null)
+        {
+            input ??= Input.ReadAllLines();
+            var height = input.Length;
+            var width = input[0].Length;
+
+            map = new int[height, width];
+
+            for (var row = 0; row < height; row++)
+            {
+                for (var col = 0; col < width; col++)
+                {
+                    map[col, row] = (int)char.GetNumericValue(input[row][col]);
+                }
+            }
+        }
+
+        public int Part1()
+        {
+            return 0;
+        }
+
+    }
+}
