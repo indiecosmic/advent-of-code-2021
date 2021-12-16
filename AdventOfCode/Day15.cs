dd7071836cbb4551460fff53e6165af95ad17acc
Mattias Rudén <mattias@indiesoft.org>
2021-12-16 10:06:09 +0100

    Day 15 part 1

--- AdventOfCode/Day15.cs
+++ AdventOfCode/Day15.cs
@@ -1,39 +1,110 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 
 namespace AdventOfCode
 {
     public class Day15
     {
-        private int[,] map;
-        private readonly (int x, int y) north = (x: 0, y: -1);
-        private readonly (int x, int y) south = (x: 0, y: 1);
-        private readonly (int x, int y) west = (x: -1, y: 0);
-        private readonly (int x, int y) east = (x: 1, y: 0);
+        private readonly int[,] map;
+        private readonly int[,] costs;
+        private readonly (int x, int y)[] directions = new[]
+        {
+            (x: 0, y: 1),
+            (x: 1, y: 0),
+            (x: 0, y: -1),
+            (x: -1, y: 0)
+        };
+        private readonly (int x, int y) destination;
+        private readonly int height;
+        private readonly int width;
+        
 
         public Day15(string[] input = null)
         {
             input ??= Input.ReadAllLines();
-            var height = input.Length;
-            var width = input[0].Length;
-
-            map = new int[height, width];
-
+            height = input.Length;
+            width = input[0].Length;
+            map = new int[width, height];
+            costs = new int[width, height];
             for (var row = 0; row < height; row++)
             {
                 for (var col = 0; col < width; col++)
                 {
                     map[col, row] = (int)char.GetNumericValue(input[row][col]);
+                    costs[col, row] = int.MaxValue;
                 }
             }
+            
+            destination = (width - 1, height - 1);
         }
 
         public int Part1()
         {
-            return 0;
+            var visitingQueue = new Queue<(int x, int y)>();
+            var source = (x: 0, y: 0);
+            costs[0, 0] = 0;
+            visitingQueue.Enqueue(source);
+
+            while (visitingQueue.Count > 0)
+            {
+                var next = visitingQueue.Dequeue();
+                foreach (var dir in directions)
+                {
+                    var target = (x: next.x + dir.x, y: next.y + dir.y);
+                    var cost = costs[next.x, next.y];
+                    if (IsValid(target.x, target.y, cost)) {
+                        visitingQueue.Enqueue((target.x, target.y));
+                        costs[target.x, target.y] = cost + map[target.x, target.y];
+                    }
+                }
+            }
+
+            return costs[destination.x, destination.y];
+        }
+
+        public int Part2()
+        {
+
+
+            var visitingQueue = new Queue<(int x, int y)>();
+            var source = (x: 0, y: 0);
+            costs[0, 0] = 0;
+            visitingQueue.Enqueue(source);
+
+            while (visitingQueue.Count > 0)
+            {
+                var next = visitingQueue.Dequeue();
+                foreach (var dir in directions)
+                {
+                    var target = (x: next.x + dir.x, y: next.y + dir.y);
+                    var cost = costs[next.x, next.y];
+                    if (IsValid(target.x, target.y, cost))
+                    {
+                        visitingQueue.Enqueue((target.x, target.y));
+                        costs[target.x, target.y] = cost + map[target.x, target.y];
+                    }
+                }
+            }
+
+            return costs[destination.x, destination.y];
+        }
+
+        private static int[,] CreateBigMap(int[,] map)
+        {
+            var height = map.GetLength(0) * 5;
+            var width = map.GetLength(1) * 5;
+            var copy = new int[map.GetLength(1), map.GetLength(0)];
+
+
+
+            return null;
+        }
+
+        private bool IsValid(int x, int y, int cost)
+        {
+            if (x >= 0 && y >= 0 && x < width && y < height && cost+map[x,y] < costs[x, y])
+                return true;
+            return false;
         }
 
     }
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -6,7 +6,7 @@ namespace AdventOfCode
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(new Day14().Part2());
+            Console.WriteLine(new Day15().Part1());
         }
     }
 }
