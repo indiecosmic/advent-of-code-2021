7a844838357a676bc1476a276a671a74fb67bc24
Mattias Rudén <mattias@indiesoft.org>
2021-12-16 22:45:01 +0100

    Day 16 draft

---
+++ AdventOfCode/Day16.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    public class Day16
+    {
+        private readonly string _input;
+
+        public Day16(string input = null)
+        {
+            _input = input ?? Input.ReadAllLines()[0];
+        }
+
+        public int Part1()
+        {
+            return 0;
+        }
+
+        public int Part2()
+        {
+            return 0;
+        }
+    }
+}
